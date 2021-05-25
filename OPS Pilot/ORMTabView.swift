84200bc760586ea90f48f1b85377f28183b22f54
Caleb Wells <calebwells@webheadtech.com>
2021-05-25 12:19:58 -0600

    Built out the bare-bones ORM view.

--- OPS Pilot/ORMTabView.swift
+++ OPS Pilot/ORMTabView.swift
@@ -8,11 +8,28 @@
 import SwiftUI
 
 struct ORMTabView: View {
+    @State private var formSelection = 0
+    
     var body: some View {
         VStack {
-            Form {
-                Section {
-                    Text("Coming soon...")
+            Picker(selection: $formSelection, label: Text("Picker")) {
+                Text("U-2").tag(0)
+                Text("T-38").tag(1)
+            }
+            .pickerStyle(SegmentedPickerStyle())
+            .padding()
+            
+            if formSelection == 0 {
+                Form {
+                    Section {
+                        Text("U-2")
+                    }
+                }
+            } else {
+                Form {
+                    Section {
+                        Text("T-38")
+                    }
                 }
             }
         }
@@ -21,6 +38,10 @@ struct ORMTabView: View {
 
 struct ORMTabView_Previews: PreviewProvider {
     static var previews: some View {
-        ORMTabView()
+        Group {
+            ORMTabView()
+                .preferredColorScheme(.dark)
+            ORMTabView()
+        }
     }
 }
